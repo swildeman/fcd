1d1f529e7467d59464fce0b9fb5e94abfa99d686
swildeman <swildeman@localhost>
2018-05-06 01:17:05 +0200

    2D unwrap algorithm based on Volkov&Zhu, Opt. Lett, 2003

--- unwrap2.m
+++ unwrap2.m
@@ -1,8 +1,63 @@
-function [ phi ] = unwrap2( phi )
-%UNWRAP2 very basic 2D unwrap, using Matlab's built-in 1D function twice
-%replace with your own favorite unwrap algorithm
+function [ ph ] = unwrap2( ph_w, roi )
+%UNWRAP2 unwrap 2D phase map
+%
+% Based on: V. Volkov and Y. Zhu, Deterministic phase unwrapping in the
+% presence of noise, Opt. Lett. (2003)
 
-phi = unwrap(unwrap(phi,[],1),[],2);
+if nargin < 2
+   hasroi = false;
+else
+   hasroi = true;
+end
+
+Nx = size(ph_w,2);
+Ny = size(ph_w,1);
+
+if hasroi
+   ph = ph_w; % don't touch regions outside roi
+   ph_w = ph_w(roi);
+end
+
+% auxilary function
+Z = exp(1i*ph_w);
+
+% second order finite difference
+if hasroi
+    [Dx,Dy] = designgrad(roi);
+    phx_w = Dx*ph_w;
+    phy_w = Dy*ph_w;
+    phx = Dx*Z;
+    phy = Dy*Z;
+else    
+    Dx = designgrad1D(Nx);
+    Dy = designgrad1D(Ny);
+    phx_w = (Dx*ph_w.').';
+    phy_w = Dy*ph_w;
+    phx = (Dx*Z.').';
+    phy = Dy*Z;
+end
+
+% Volkov&Zhu's trick to calculate location of phase jumps
+phx = real(phx./(1i*Z));
+phy = real(phy./(1i*Z));
+jx = phx-phx_w;
+jy = phy-phy_w;
+
+if hasroi % integrate by matrix inversion
+  j = [0;[Dx(:,2:end);Dy(:,2:end)] \ [jx;jy]];
+else % integrate in fourier domain
+  j = fftinvgrad(jx,jy,'gradtype','diff');
+end
+
+% make sure correction is integer number of 2pi 
+j = 2*pi*round(j/(2*pi));
+
+% unwrap phase
+if hasroi
+   ph(roi) = ph_w + j;
+else
+   ph = ph_w + j;
+end
 
 end
 
